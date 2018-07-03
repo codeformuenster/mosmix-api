341beed01fa6b1039eca9b61b4f7560dea62cdd6
ubergesundheit <ubergesundheit@googlemail.com>
2018-07-03 20:14:45 +0200

    use new forecasts_for_place_id function

--- api/queries/forecasts/forecast.read.sql
+++ api/queries/forecasts/forecast.read.sql
@@ -1,1 +1,1 @@
-SELECT ST_X(p.the_geom) AS lon, ST_Y(p.the_geom) AS lat, ST_Z(p.the_geom) AS alt, f.* FROM forecast_places AS p JOIN forecasts_all AS f ON p.id = f.place_id WHERE id = '{{.id}}'
+SELECT * FROM forecasts_for_place_id('{{.id}}')
